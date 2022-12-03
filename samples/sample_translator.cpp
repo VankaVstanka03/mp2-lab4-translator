25c1034bbee47abd59aeeb1514b162a2a05d6fad
VankaVstanka03 <vvstanka39@gmail.com>
2022-12-04 02:26:14 +0300

    empty files

---
+++ samples/sample_translator.cpp
@@ -0,0 +1,9 @@
+﻿#include "translator.h"
+#include <iostream>
+
+int main() {
+	Lexeme a("Jesus1+2");
+	std::cout << a.all << " " << a.numbers << " " << a.operators << " " << a.variables << std::endl;
+
+	return 0;
+}
